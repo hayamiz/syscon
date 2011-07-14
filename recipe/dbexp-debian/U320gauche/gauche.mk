acc328a6deb3fe948b59d95f8e350ff32fcb9555
Yuto Hayamizu <y.hayamizu@gmail.com>
2011-07-15 02:16:35 +0900

    add gauche task

---
+++ recipe/dbexp-debian/U320gauche/gauche.mk
@@ -0,0 +1,18 @@
+
+include $(SYSCON_INCLUDE)/common.mk
+
+$(SYSCON_TARGET):
+	cd /tmp && \
+	rm -rf Gauche-* && \
+	tar xf $(shell pwd)/Gauche-*.tgz && \
+	cd Gauche-* && \
+	./configure --prefix=$(SYSCON_PREFIX) \
+	  --enable-multibyte=utf-8 \
+	  --enable-threads=pthreads \
+	  --enable-ipv6 \
+	  CFLAGS="-I$(SYSCON_PREFIX)/include" \
+	  LDFLAGS="-L$(SYSCON_PREFIX)/lib" && \
+	make -j8 && \
+	make install
+	$(FINISH)
+

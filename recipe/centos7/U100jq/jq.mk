a1959b047d4e09879824b79afef1afd69b78f4c9
Yuto Hayamizu <y.hayamizu@gmail.com>
2016-12-12 20:57:00 +0900

    removed unnecessary pkgs from centos7 recipe

--- recipe/centos7/U100jq/jq.mk
+++ recipe/centos7/U100jq/jq.mk
@@ -1,7 +1,10 @@
 
 include $(SYSCON_INCLUDE)/common.mk
 
-$(SYSCON_TARGET):
+$(SYSCON_PREFIX)/bin:
+	mkdir -p $@
+
+$(SYSCON_TARGET): $(SYSCON_PREFIX)/bin
 	cp jq-linux64 $(SYSCON_PREFIX)/bin/jq && \
 	$(FINISH)
 
